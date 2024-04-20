ef77e141430cd56604e35825d914f603e9a28962
Kristen Cheung <krcheung9@gmail.com>
2024-04-20 16:26:37 -0400

    add updated functions

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# sample-github-integ
+This is a template for new Hypermode projects. For more information on project configuration, [reference the docs](https://docs.hypermode.com/manifest).
--- functions/assembly/index.ts
+++ functions/assembly/index.ts
@@ -1,9 +1,6 @@
-import { JSON } from "json-as";
-import { model } from "@hypermode/functions-as";
+import { model, ClassificationResult } from "@hypermode/functions-as";
 
-const modelId = "msp-523a8138-76b3-406f-9386-c53aa9b23c44";
-//
-export function testClassifier(text: string): string {
-  const results = model.classifyText(modelId, text);
-  return JSON.stringify(results);
+export function testClassifier(text: string): ClassificationResult {
+  const modelName = "sentiment-classifier";
+  return model.classifyText(modelName, text);
 }
